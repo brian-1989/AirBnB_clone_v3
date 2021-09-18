c69178ea0a0bc1c4ba849dc4b52bcf40b8e33179
dianaparr <dianaparra.9611@gmail.com>
2021-09-18 12:02:02 -0500

    Updating request method post and put, and improve the return so that is only values dictionary

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,10 +5,11 @@ from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
 from models.state import State
-import json
+
 
 @app_views.route("/states", methods=['GET', 'POST'], strict_slashes=False)
-@app_views.route("/states/<state_id>", methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+@app_views.route("/states/<state_id>", methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
 def all_states(state_id=None):
     """ this function retrieves the list of all State objects """
     if request.method == 'GET':
@@ -27,30 +28,32 @@ def all_states(state_id=None):
             abort(404)
     if request.method == 'DELETE':
         obj_state = storage.get(State, state_id)
-        if obj_state != None:
+        if obj_state:
             storage.delete(obj_state)
             storage.save()
             return jsonify(dict()), 200
         abort(404)
     if request.method == 'POST':
         conv_body = request.get_json()
-        for key in conv_body.keys():
-            if key == 'name':
-                new_inst = State(name=conv_body.get('name'))
-                storage.new(new_inst)
-                storage.save()
-                return jsonify(new_inst.values())
-        abort(400, description="Missing name")
-        abort(400, description="Not a JSON")
-    if request.method == 'PUT':
+        if 'name' not in conv_body:
+            abort(400, description="Missing name")
         try:
-            conv_body = request.get_json()
+            new_inst = State(name=conv_body.get('name'))
+            storage.new(new_inst)
+            storage.save()
+            return jsonify(new_inst.to_dict()), 201
         except Exception:
             abort(400, description="Not a JSON")
-        new_inst = storage.get(State, state_id)
-        if new_inst is not None:
-            for key, value in conv_body.items():
-                setattr(new_inst, key, value)
-            return jsonify(str(new_inst))
-        else:
-            abort(404)
+    if request.method == 'PUT':
+        conv_body = request.get_json()
+        try:
+            new_inst = storage.get(State, state_id)
+            if new_inst is not None:
+                for key, value in conv_body.items():
+                    setattr(new_inst, key, value)
+                new_inst.save()
+                return jsonify(new_inst.to_dict())
+            else:
+                abort(404)
+        except Exception:
+            abort(400, description="Not a JSON")
