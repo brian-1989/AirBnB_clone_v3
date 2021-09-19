bd9d11849dabaf5d66f95cbb38956b36cbf89f30
Brian Zapata <dzapata397@gmail.com>
2021-09-19 16:56:49 -0500

    The GET, DELETE, POST and PUT methods was added in the users places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,7 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from api.v1.app import show_error
 
 
 @app_views.route("/cities/<city_id>/places", methods=['GET', 'POST'],
@@ -14,12 +15,64 @@ from models.user import User
 def all_places_of_city(city_id=None):
     """ this function retrieves the list of all places objects
         belonging to a specific city_id """
+    if request.method == 'GET':
+        _cities = storage.get(City, city_id)
+        if _cities is not None:
+            list_places = list()
+            for city in _cities.places:
+                list_places.append(city.to_dict())
+            return jsonify(list_places)
+        abort(404)
+    if request.method == 'POST':
+        _cities = storage.get(City, city_id)
+        if _cities is None:
+            abort(404)
+        try:
+            conv_body = request.get_json()
+            if 'user_id' not in conv_body:
+                return "Missing user_id\n", 400
+            _user = storage.get(User, conv_body.get('user_id'))
+            if _user is None:
+                return show_error(404)
+            if 'name' not in conv_body:
+                return "Missing name\n", 400
+            new_inst = Place(name=conv_body.get('name'), user_id=conv_body.get(
+                'user_id'), city_id=_cities.id)
+            storage.new(new_inst)
+            storage.save()
+            return jsonify(new_inst.to_dict()), 201
+        except:
+            abort(400, description="Not a JSON")
 
-
-# En POST: hacer lo mismo que hicimos en cities.py (líneas 25, 26, 27 y 33) con City y con User
-# (pues tiene llaves foraneas de cada tabla: city_id y user_id)
 
 @app_views.route("/places/<place_id>", methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
 def all_places(place_id=None):
     """ this function retrieves the list of all Place objects """
+    if request.method == 'GET':
+        _place = storage.get(Place, place_id)
+        if _place is None:
+            abort(404)
+        return jsonify(_place.to_dict())
+    if request.method == 'DELETE':
+        obj_place = storage.get(Place, place_id)
+        if obj_place:
+            storage.delete(obj_place)
+            storage.save()
+            return jsonify(dict()), 200
+        abort(404)
+    if request.method == 'PUT':
+        new_inst = storage.get(Place, place_id)
+        if not new_inst:
+            abort(404)
+        try:
+            list_ignore = [
+                'id', 'created_at', 'updated_at', 'user_id', 'city_id']
+            conv_body = request.get_json()
+            for key, value in conv_body.items():
+                if key not in list_ignore:
+                    setattr(new_inst, key, value)
+            new_inst.save()
+            return jsonify(new_inst.to_dict()), 200
+        except:
+            abort(400, description="Not a JSON")
