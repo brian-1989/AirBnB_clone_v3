69662fa7af117d8b6368ef9308f42351f822ed87
dianaparr <dianaparra.9611@gmail.com>
2021-09-20 10:55:03 -0500

    Uploading task 13 adding CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """ Module that represents the main application """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -11,6 +12,7 @@ PORT = getenv('HBNB_API_PORT')
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors_inst = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
