b98dafbbe50f2039e7afc3f87ec6764d95f89f25
dianaparr <dianaparra.9611@gmail.com>
2021-09-20 23:41:49 -0500

    Changing instance to storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -93,7 +93,7 @@ class TestFileStorage(unittest.TestCase):
         new_inst = State(name='Illinois')
         _id = new_inst.id
         # new_inst.new(new_inst)
-        new_inst.save()
+        storage.save()
         self.assertEqual(storage.get(State, _id), new_inst)
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
@@ -101,6 +101,6 @@ class TestFileStorage(unittest.TestCase):
         """Test to method count"""
         new_inst = State(name='Illinois')
         # new_inst.new(new_inst)
-        new_inst.save()
+        storage.save()
         all_objects = len(storage.all(State))
         self.assertEqual(all_objects, storage.count(State))
