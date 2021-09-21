14e726152264e02c7eb067bb318de492a0064378
Brian Zapata <dzapata397@gmail.com>
2021-09-20 22:06:03 -0500

    the 0.0.0.0 was added in the cors instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
